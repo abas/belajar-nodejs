2e1803cebbed321dd5c1cde9c33ddea008a6c878
ahmadbasir <akhmadbasir5@gmail.com>
2017-08-02 20:34:30 +0700

    still Querying :v

---
+++ alpha/mySQL/selectWhere.js
@@ -0,0 +1,34 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'ahmadbasir',
+  database:'nodejs'
+});
+
+var getClose = function(messages) {
+  return messages;
+}
+
+con.connect(function(err) {
+  if (err) {
+    throw err;
+  }
+  console.log('connected!');
+  console.log('Query loaded : ');
+
+  // SELECT where there are have Char 'A' on the front
+  // var SQLstate = "SELECT * FROM mhs WHERE nama LIKE 'A%'"
+
+  //SELECT with different rule
+  var SQLstate = "SELECT * FROM mhs WHERE nama LIKE ?"
+  con.query(SQLstate,'A%',function(err,result,fields) {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+    con.end();
+    console.log(getClose('disconnected!'));
+  })
+});
