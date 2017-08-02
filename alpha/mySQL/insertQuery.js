c80205c9a8e23a91e72328a2ecbd83eedd5ba8ff
ahmadbasir <akhmadbasir5@gmail.com>
2017-08-03 03:06:35 +0700

    updated on mysql : insertQuery.js

--- alpha/mySQL/insertQuery.js
+++ alpha/mySQL/insertQuery.js
@@ -12,14 +12,19 @@ con.connect(function(err) {
     throw err;
   }console.log('connected!');
 
-  var SQLstate = "INSERT INTO mhs (nama,nim)"+
-                 " VALUES ('Antonius Billy','A11.2014.08170')";
-
-  con.query(SQLstate,function(err,result) {
-    if (err) {
-      throw err;
-    }console.log('Query Inserted!');
-    // }console.log('Table Deleted!'); // message
-    con.end();
-  })
+  var SQLstate = [
+                ("INSERT INTO mhs (nama,nim)"+
+                 " VALUES ('Nganu Iki','A11.2014.01863')"),
+                ("INSERT INTO mhs (nama,nim)"+
+                 " VALUES ('Sopo Kuwi','A12.2015.09128')")
+               ];
+  var sum = 0;
+  for(index in SQLstate){
+    con.query(SQLstate[index],function(err,result) {
+      if (err) throw err;
+    })
+    sum++;
+  }
+  console.log(sum,' Query Inserted!');
+  con.end();
 });
