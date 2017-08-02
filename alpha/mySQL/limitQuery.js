4e0e6fbf0a2eee31fcffd309f3033f3e71c3b676
ahmadbasir <akhmadbasir5@gmail.com>
2017-08-03 03:16:25 +0700

    update mySQL : FINISH

--- alpha/mySQL/limitQuery.js
+++ alpha/mySQL/limitQuery.js
@@ -12,12 +12,21 @@ con.connect(function(err) {
     throw err;
   }console.log('connected!');
 
-  var LIMITstate = "SELECT * FROM mhs LIMIT 2 OFFSET 1";
+  // var LIMITstate = "SELECT * FROM mhs LIMIT 2 OFFSET 1";
+  // they'll just get 2 query on start on 1 offset from first index
+  // con.query(LIMITstate,function(err,result){
+  //   if(err) throw err;
+  //   console.log(result);
+  // })
+
+  var LIMITstate = "SELECT * FROM mhs LIMIT 1,2";
+  // same like up there
   // they'll just get 2 query on start on 1 offset from first index
   con.query(LIMITstate,function(err,result){
     if(err) throw err;
     console.log(result);
   })
+
   // var LIMITstate = "SELECT * FROM mhs LIMIT 2"; // they'll just get 2 query
   // con.query(LIMITstate,function(err,result){
   //   if(err) throw err;
